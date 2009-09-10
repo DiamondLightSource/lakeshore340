a358eee190f55bc984ca6b0a837f818daab8f708
Matthew Pearson <Matthew.Pearson@diamond.ac.uk>
2009-09-10 17:06:15 +0000

    lakeshore340: moves test cases into cases.py and load them dynamically in the test suite class.

---
+++ etc/test/cases.py
@@ -0,0 +1,63 @@
+from pkg_resources import require
+require('dls.autotestframework')
+from dls.autotestframework import *
+
+class ls340CaseBase(TestCase):
+   """
+   Base class for all lakeshore340 test cases.
+   This is where the base PV is hardcoded to be 'mp49:ls340sim'
+   """
+   
+   def __init__(self, A):
+      TestCase.__init__(self, A)
+      self.__pvbase = "mp49:ls340sim"
+
+   def getPVBase(self):
+      return self.__pvbase
+
+
+class ls340CaseGetID(ls340CaseBase):
+   """
+   Test case to read the ID string.
+   The test just checks that the string is in the correct format.
+   """
+   
+   def runTest(self):
+      
+      pv = self.getPVBase() + ":ID"
+      
+      val = self.getPv(pv)
+
+      regex = re.compile('^LSCI,MODEL340,.{6},.{8}')
+      if (not(regex.match(val))):
+         self.fail(str("ID string did not match expected format. Got: " + val))
+
+
+class ls340CaseHTR(ls340CaseBase):
+   """
+   Test case to read HTR value.
+   Checks that it is in valid range of (0-100)
+   """
+
+   def runTest(self):
+      pv = self.getPVBase() + ":HTR"
+      self.verifyPvInRange(pv, 0, 100)
+
+
+class ls340CaseSETP(ls340CaseBase):
+   """
+   Test case to set and read the SETP PV.
+   It does not check that the readback is correct (don't know what range to check..)
+   """
+
+   def runTest(self):
+      pv = self.getPVBase() + ":SETP"
+      pvSet = pv + "_S"
+
+      self.putPv(pvSet, 23)
+      val = self.getPv(pv)
+      #More puts to check it still works
+      self.putPv(pvSet, -10)
+      self.putPv(pvSet, 100)
+      self.putPv(pvSet, 350)
+      self.putPv(pvSet, 20)
--- etc/test/lakeshore340_test.py
+++ etc/test/lakeshore340_test.py
@@ -6,10 +6,22 @@ from pkg_resources import require
 require('dls.autotestframework')
 from dls.autotestframework import *
 
+from cases import *
+import pyclbr
+
 ################################################
 # Test suite for the lakeshore340 temperature controller.
     
 class ls340TestSuite(TestSuite):
+
+   def loadCasePlugins(self):
+      classes = pyclbr.readmodule("cases")
+      for c in classes:
+         if not (c.endswith("Base")):
+            classobj = eval(c)
+            classinstance = classobj(self)
+         
+
    
    def createTests(self):
       # Define the targets for this test suite
@@ -20,72 +32,10 @@ class ls340TestSuite(TestSuite):
              simDevices=[SimDevice("controller1", 9001)],
              environment=[('EPICS_CA_REPEATER_PORT','6065'),('EPICS_CA_SERVER_PORT','6064')],
              guiCmds=['edm -m "lakeshore340=mp49:ls340sim" -x data/lakeshore340.edl'])
+
+      self.loadCasePlugins()
       
-      # The tests
-      ls340CaseGetID(self)
-      ls340CaseSETP(self)
-      
-      
-       
-################################################
-# Intermediate test case class that provides some utility functions
-# for this suite
-
-
-class ls340CaseBase(TestCase):
-   """
-   Base class for all lakeshore340 test cases.
-   This is where the base PV is hardcoded to be 'mp49:ls340sim'
-   """
-   
-   def __init__(self, A):
-      TestCase.__init__(self, A)
-      self.__pvbase = "mp49:ls340sim"
-
-   def getPVBase(self):
-      return self.__pvbase
-      
-
-
-################################################
-# Test cases
-
-
-class ls340CaseGetID(ls340CaseBase):
-   """
-   Test case to read the ID string.
-   The test just checks that the string is in the correct format.
-   """
-   
-   def runTest(self):
-      
-      pv = self.getPVBase() + ":ID"
-      
-      val = self.getPv(pv)
-
-      regex = re.compile('^LSCI,MODEL340,.{6},.{8}')
-      if (not(regex.match(val))):
-         self.fail(str("ID string did not match expected format. Got: " + val))
-
-      
-class ls340CaseSETP(ls340CaseBase):
-   """
-   Test case to set and read the SETP PV.
-   It does not check that the readback is correct (don't know what range to check..)
-   """
-
-   def runTest(self):
-      pv = self.getPVBase() + ":SETP"
-      pvSet = pv + "_S"
-
-      self.putPv(pvSet, 23)
-      val = self.getPv(pv)
-      #Couple more puts to check it still works
-      self.putPv(pvSet, 0)
-      self.putPv(pvSet, 100)
-   
-
-
+     
 
 
 ################################################
